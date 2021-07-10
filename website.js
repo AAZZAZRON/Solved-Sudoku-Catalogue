9ab50c6ab04cc9d6684f049ec8a611ee632beb9b
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-07-09 20:11:42 -0400

    get cookie

--- website.js
+++ website.js
@@ -1,4 +1,21 @@
 var outer = document.getElementById("items");
+var puzzles = [];
 window.onload = function() {
+    getPuzzles();
+}
 
+
+function getPuzzles() { // not tested
+    var tmp = getCookie("items").split("øπø");
+    if (tmp == "") return;
+    for (let val in tmp) {
+        val = val.split("√∫√");
+        val[6] = val[6].split("∑∑∑");
+        puzzles.push(val);
+    }
+    return;
+}
+
+function render() {
+    
 }
