94c5bc86d132b47ff45f063a79636d1dd2835cac
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-07-13 16:48:15 -0400

    display

--- website.js
+++ website.js
@@ -1,13 +1,3 @@
-var outer = document.getElementById("items");
-var puzzles = [];
 window.onload = function() {
-}
 
-document.getElementById("admin").onclick = function() {
-    var password = window.prompt("What is the admin password?");
-    if (password === PASSWORD) console.log("yes");
-}
-
-function render() {
-    
 }
