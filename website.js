2a8509a44d7ec4e5e0e3969d06b7dafff986ba09
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-07-14 12:29:05 -0400

    get filters

--- website.js
+++ website.js
@@ -1,3 +1,16 @@
+var filters = new Set();
+var num;
+var sudokus;
 window.onload = function() {
-
+    num = document.getElementById("posts").children[0].id.split("-")[1];
+    num = parseInt(num);
+    sudokus = document.getElementById("posts").children;
+    for (let i = 0; i < num; i += 1) {
+        let localTags = sudokus[i].classList.value.split(" ");
+        for (let j = 0; j < localTags.length; j += 1) {
+            filters.add(localTags[j]);
+        }
+    }
+    filters = Array.from(filters);
+    console.log(filters);
 }
